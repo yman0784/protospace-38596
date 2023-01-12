4791933d20fd18fbcf02ac406006e371711e2a2e
yman0794 <tykesam@yahoo.co.jp>
2023-01-12 22:46:48 +0900

    Revert "first commit"
    This reverts commit 61728f5969f21a258d94b6f13897a9470a392396.

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :prototype
-
-  validates :content, presence: true
-end
--- app/models/prototype.rb
+++
@@ -1,8 +0,0 @@
-class Prototype < ApplicationRecord
-  belongs_to :user
-  has_many :comments, dependent: :destroy
-
-  has_one_attached :image
-
-  validates :image, :title, :catch_copy, :concept, presence: true
-end
